5d17b0a4613ca9f4f1cb6ec9b1e55a6cf452f3ce
Bauyrzhan Marat <baurrm@Baur-MacBook.local>
2023-04-05 02:27:44 +0200

    Creating new struct and organising code

---
+++ Chapter 11/FoodPin/Model/Restaurant.swift
@@ -0,0 +1,30 @@
+//
+//  Restaurant.swift
+//  FoodPin
+//
+//  Created by Bauyrzhan Marat on 04.04.2023.
+//
+
+import Foundation
+
+struct Restaurant: Hashable {
+    var name: String
+    var type: String
+    var location: String
+    var image: String
+    var isFavorite: Bool
+    
+    init(name: String, type: String, location: String, image: String, isFavorite: Bool) {
+        self.name = name
+        self.type = type
+        self.location = location
+        self.image = image
+        self.isFavorite = isFavorite
+    }
+    
+//  If create an empty instance, saves time Restaurant() instead of  Restaurant(name: "", type: "", location: "", image: "", isFavorite: false)
+    init() {
+        self.init(name: "", type: "", location: "", image: "", isFavorite: false)
+    }
+    
+}
